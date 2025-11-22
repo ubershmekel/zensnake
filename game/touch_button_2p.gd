51575818584077ec10aaaa8444e83f5e1f7df9a4
Yuval Greenfield <ubershmekel@gmail.com>
2025-11-21 22:24:47 -0800

    Show 2 player games where to tap

---
+++ game/touch_button_2p.gd
@@ -0,0 +1,22 @@
+extends TouchScreenButton
+
+const GREEN := Color8(0x75, 0xbe, 0x99)
+const ORANGE := Color8(0xcd, 0x61, 0x3d)
+
+var _base_color: Color = Color(1, 1, 1, 1)
+
+func _ready() -> void:
+	match name:
+		"LeftButton":
+			_base_color = GREEN
+		"RightButton":
+			_base_color = ORANGE
+	modulate = _base_color
+
+
+func _on_pressed() -> void:
+	create_tween().tween_property(self, "modulate", Color(1.7, 1.7, 1.7, 1.0), 0.05)
+
+
+func _on_released() -> void:
+	create_tween().tween_property(self, "modulate", _base_color, 0.12)
