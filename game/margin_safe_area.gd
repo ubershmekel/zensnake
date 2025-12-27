1d4de56221bc5424c9c1d7187b0eac8fb88320f7
Yuval Greenfield <ubershmekel@gmail.com>
2025-12-26 23:03:57 -0800

    Fixed safe area on windows (the title screen looked blank)

--- game/margin_safe_area.gd
+++ game/margin_safe_area.gd
@@ -11,11 +11,13 @@ func _ready() -> void:
 func _apply_safe_area() -> void:
 	var safe: Rect2i = DisplayServer.get_display_safe_area()
 	var win: Vector2i = DisplayServer.window_get_size()
+	var win_pos: Vector2i = DisplayServer.window_get_position()
 
-	var left   := safe.position.x
-	var top    := safe.position.y + extra_top_px
-	var right  := win.x - (safe.position.x + safe.size.x)
-	var bottom := win.y - (safe.position.y + safe.size.y)
+	# Translate the safe area (screen space) into window-local margins
+	var left: int   = max(0, safe.position.x - win_pos.x)
+	var top: int    = max(0, safe.position.y - win_pos.y) + extra_top_px
+	var right: int  = max(0, (win_pos.x + win.x) - (safe.position.x + safe.size.x))
+	var bottom: int = max(0, (win_pos.y + win.y) - (safe.position.y + safe.size.y))
 
 	add_theme_constant_override("margin_left", left)
 	add_theme_constant_override("margin_top", top)
