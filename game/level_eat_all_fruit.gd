73c492604ef2d35484237ae2d97f111c0749bca2
Yuval Greenfield <ubershmekel@gmail.com>
2025-12-29 09:51:59 -0800

    Make level_eat_all_fruit more robust to node tree changes Done that by marking the fruit scene as part of the fruits group and using that instead of "children" Groups are a very cool idea, thank you godot

--- game/level_eat_all_fruit.gd
+++ game/level_eat_all_fruit.gd
@@ -4,16 +4,13 @@ var _remaining_fruits: int = 0
 
 func _ready() -> void:
 	_count_fruits()
-	for child in get_children():
-		if child is FruitClass:
-			child.eaten.connect(_on_fruit_eaten)
-			child.eaten_animation_done.connect(_on_fruit_eaten_animation_done)
+	var fruits = get_tree().get_nodes_in_group("fruits")
+	for fruit in fruits:
+		fruit.eaten.connect(_on_fruit_eaten)
+		fruit.eaten_animation_done.connect(_on_fruit_eaten_animation_done)
 
 func _count_fruits() -> void:
-	_remaining_fruits = 0
-	for child in get_children():
-		if child is FruitClass:
-			_remaining_fruits += 1
+	_remaining_fruits = get_tree().get_nodes_in_group("fruits").size()
 
 func _on_fruit_eaten(eater: Node, fruit_data: FruitData) -> void:
 	# Emit signal so game can react to fruit being eaten
